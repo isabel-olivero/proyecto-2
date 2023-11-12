68f77eacc4a2b31752dff827a77c7a39dbd06ce9
aarguellocedeno <124204583+aarguellocedeno@users.noreply.github.com>
2023-11-12 14:08:42 -0500

    actualización

--- avances/vuelo.py
+++ avances/vuelo.py
@@ -1,18 +1,35 @@
 class vuelo:
-    def __init__(self,des,date,s,n):
+    def __init__(self,des,date,s,n,a,j,h):
         self.sillasV= s
         self.idVuelo = n
-        self.pasajeros =[]
+        #self.pasajeros =[]
         self.des= des
         self.fecha = date
         self.origen = "Cali"
+        self.sillasA = a
+        self.sillasJ = j
+        self.sillasH =h
+        self.pasajerosA = []
+        self.pasajerosH = []
+        self.pasajerosJ = []
 
-
-    def reservar(pasajero):
-        if (self.sillasV > 0):
-            sillas -= 1
-            self.pasajeros.append(pasajero)
-
-        else:
-            print("No es posible reservar")
+    def reservar(self,pasajero,selec):
+        if(selec=="Avion"):
+            if(self.sillasA>0):
+                self.sillasA -= 1
+                self.pasajerosA.append(pasajero)
+            else:
+                print("No es posible reservar")
+        elif(selec=="Helicoptero"):
+            if(self.sillasH>0):
+                self.sillasH -= 1
+                self.pasajerosH.append(pasajero)
+            else:
+                print("No es posible reservar")
+        elif(selec=="Jet"):
+            if(self.sillasJ>0):
+                self.sillasJ -= 1
+                self.pasajerosJ.append(pasajero)
+            else:
+                print("No es posible reservar")
             

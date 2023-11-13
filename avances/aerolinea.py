a2a312f8e2b51486f170b769d82393c52e3610d3
aarguellocedeno <124204583+aarguellocedeno@users.noreply.github.com>
2023-11-12 23:55:25 -0500

    cambiooooo   aerolinea.py

--- avances/aerolinea.py
+++ avances/aerolinea.py
@@ -1,7 +1,4 @@
-import vuelo
-import aeronave
-import persona
-import view
+import viewGeneral
 import model
 
 
@@ -15,27 +12,27 @@ class aerolinea:
         self.tripulacion=[]
         self.pasajeros=[]
 
-    def agregarVuelo(vuelo): #agrega un nuevo vuelo a la lista de vuelo de la aerolinea
+    def agregarVuelo(self,vuelo): #agrega un nuevo vuelo a la lista de vuelo de la aerolinea
         self.vuelos.append(vuelo)
 
-    def mostrarVuelos():# muestra los vuelos existententes en la aerolinea
+    def mostrarVuelos(self):# muestra los vuelos existententes en la aerolinea
         for t in range(len(self.vuelos)):
             self.vuelos[t].mostrarVuelo()
 
-    def agregarTripulante(tripulante): #agrega un nuevo tripulante a la lista de tripulantes de la aerolinea
+    def agregarTripulante(self,tripulante): #agrega un nuevo tripulante a la lista de tripulantes de la aerolinea
         self.tripulacion.append(tripulante)
 
-    def selecVuelo(num,pasajero):#agrega un nuevo pasajero al vuelo seleccionado y al historial de pasajeros en la aerolinea
+    def selecVuelo(self,num,pasajero):#agrega un nuevo pasajero al vuelo seleccionado y al historial de pasajeros en la aerolinea
         self.vuelos[num].reservarCupo(pasajero)
         self.pasajeros.append(pasajero)
 
 
 
-    def reservarAeronave(aeronave): #agrega una nueva aeronave a la aerolinea
+    def reservarAeronave(self,aeronave): #agrega una nueva aeronave a la aerolinea
         self.aeronaves.append(aeronave)
 
 
- def asignarTripulacion(vuelo):
+    def asignarTripulacion(self,vuelo):
         reLi = self.model.retornarT()
         iden = self.view.pedirId(len(reLi))
         p = reLi[iden]
@@ -45,50 +42,21 @@ class aerolinea:
         az = vuelo.az 
         pol = vuelo.pol
      
-        if((cap = 0) and (puesto == 'Piloto')):
+        if((cap == 0) and (puesto == 'Piloto')):
             vuelo.cap = 1
             vuelo.tripulacion.append(p)
-        elif((copi = 0) and(puesto == 'Copiloto')):
+        elif((copi == 0) and(puesto == 'Copiloto')):
             vuelo.copi = 1
             vuelo.tripulacion.append(p)
-        elif((az = 0) and(puesto == 'Tripulante de cabina')):
+        elif((az == 0) and(puesto == 'Tripulante de cabina')):
             vuelo.az = 1
             vuelo.tripulacion.append(p)
-        elif((pol=0) and (puesto == 'Policia aereo')):
+        elif((pol == 0) and (puesto == 'Policia aereo')):
             vuelo.pol = 1
             vuelo.tripulacion.append(p)
             
-        if((cap = 1) and (copi = 1) and (az = 1) and (pol = 1)):
+        if((cap == 1) and (copi == 1) and (az == 1) and (pol == 1)):
             print("Tripulacion completada")
 
 
-
-
-
-    # def asignarTripulacion(vuelo):
-    #     reLi = self.model.retornarT()
-    #     iden = self.view.pedirId(len(reLi))
-    #     p = reLi[iden]
-    #     puesto = p.puesto
-
-    #     tripu = []
-    #     cap,copi,az,k= 0,0,0,0
-    #     while((k < (len(self.tripulacion))) and ((cap = 0)or (copi= 0) or(az = 0) ):
-    #         if((cap = 0) and (self.tripulacion[j].cargo()== "capitan")):
-    #             cap = 1
-    #             tripu.append(self.tripulacion[j])
-    #         elif((copi = 0) and(self.tripulacion[j].cargo()== "copiloto")):
-    #             copi= 1
-    #             tripu.append( self.tripulacion[j])
-    #         elif((az = 0) and(self.tripulacion[j].cargo()== "azafata")):
-    #             az=1
-    #             tripu.append( self.tripulacion[j])
-    #         k += 1
-        
-    #     if((cap = 1)and (copi= 1) and(az = 1)):
-    #         vuelo.tripulacion = tripu
-    #     else:
-    #         print("No es posible asignar una tripulacion")
-    
-    
 

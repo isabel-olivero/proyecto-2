c94bdbcb84099ec1709a30d5dc7ec316c271e09a
aarguellocedeno <124204583+aarguellocedeno@users.noreply.github.com>
2023-11-13 11:14:29 -0500

    Update aerolinea.py

--- avances/aerolinea.py
+++ avances/aerolinea.py
@@ -4,59 +4,54 @@ import model
 
 class aerolinea:
     def __init__(self, name):
-        self.nombre =name
+        self.nombre = name
         self.view = viewGeneral.viewGeneral()
         self.model = model.modelo()
         self.vuelos = []
-        self.aeronaves= []
-        self.tripulacion=[]
-        self.pasajeros=[]
+        self.aeronaves = []
+        self.tripulacion = []
+        self.pasajeros = []
 
-    def agregarVuelo(self,vuelo): #agrega un nuevo vuelo a la lista de vuelo de la aerolinea
+    def agregarVuelo(self, vuelo):  # agrega un nuevo vuelo a la lista de vuelo de la aerolinea
         self.vuelos.append(vuelo)
 
-    def mostrarVuelos(self):# muestra los vuelos existententes en la aerolinea
+    def mostrarVuelos(self):  # muestra los vuelos existententes en la aerolinea
         for t in range(len(self.vuelos)):
             self.vuelos[t].mostrarVuelo()
 
-    def agregarTripulante(self,tripulante): #agrega un nuevo tripulante a la lista de tripulantes de la aerolinea
+    def agregarTripulante(self, tripulante):  # agrega un nuevo tripulante a la lista de tripulantes de la aerolinea
         self.tripulacion.append(tripulante)
 
-    def selecVuelo(self,num,pasajero):#agrega un nuevo pasajero al vuelo seleccionado y al historial de pasajeros en la aerolinea
+    def selecVuelo(self, num,
+                   pasajero):  # agrega un nuevo pasajero al vuelo seleccionado y al historial de pasajeros en la aerolinea
         self.vuelos[num].reservarCupo(pasajero)
         self.pasajeros.append(pasajero)
 
-
-
-    def reservarAeronave(self,aeronave): #agrega una nueva aeronave a la aerolinea
+    def reservarAeronave(self, aeronave):  # agrega una nueva aeronave a la aerolinea
         self.aeronaves.append(aeronave)
 
-
-    def asignarTripulacion(self,vuelo):
+    def asignarTripulacion(self, vuelo):
         reLi = self.model.retornarT()
         iden = self.view.pedirId(len(reLi))
         p = reLi[iden]
         puesto = p.puesto
         cap = vuelo.cap
         copi = vuelo.copi
-        az = vuelo.az 
+        az = vuelo.az
         pol = vuelo.pol
-     
-        if((cap == 0) and (puesto == 'Piloto')):
+
+        if ((cap == 0) and (puesto == 'Piloto')):
             vuelo.cap = 1
             vuelo.tripulacion.append(p)
-        elif((copi == 0) and(puesto == 'Copiloto')):
+        elif ((copi == 0) and (puesto == 'Copiloto')):
             vuelo.copi = 1
             vuelo.tripulacion.append(p)
-        elif((az == 0) and(puesto == 'Tripulante de cabina')):
+        elif ((az == 0) and (puesto == 'Tripulante de cabina')):
             vuelo.az = 1
             vuelo.tripulacion.append(p)
-        elif((pol == 0) and (puesto == 'Policia aereo')):
+        elif ((pol == 0) and (puesto == 'Policia aereo')):
             vuelo.pol = 1
             vuelo.tripulacion.append(p)
-            
-        if((cap == 1) and (copi == 1) and (az == 1) and (pol == 1)):
+
+        if ((cap == 1) and (copi == 1) and (az == 1) and (pol == 1)):
             print("Tripulacion completada")
-
-
-

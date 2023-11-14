cfe259b3467e154c897aaa0497275cdb7e0f12ab
aarguellocedeno <124204583+aarguellocedeno@users.noreply.github.com>
2023-11-14 00:14:09 -0500

    Update aeronave.py

--- avances/aeronave.py
+++ avances/aeronave.py
@@ -4,13 +4,14 @@ import streamlit as st
 import time
 
 class Aeronave:
-    def __init__(self, tipo,velocidad, cantPas, autonomia, yearFab, estado, ubi, marca, modelo, destino,cap, copi, az, pol):
+    def __init__(self, tipo,velocidad, id,cantPas, autonomia, yearFab, estado, ubi, marca, modelo, destino,propietario,cap, copi, az, pol):
         self.velocidad = velocidad
         self.cantPas = cantPas
         self.autonomia = autonomia
         self.yearFab = yearFab
         self.estado = estado
         self.ubi = ubi
+        self.id = id
         self.marca = marca
         self.modelo = modelo
         self.destino = destino
@@ -25,9 +26,9 @@ class Aeronave:
 
 
 class Avion(Aeronave):
-    def __init__(self, velocidad, cantPas, autonomia, yearFab, estado, ubi, marca, modelo, destino,tipo, cap, copi, az, pol,
+    def __init__(self, velocidad, cantPas,id, autonomia, yearFab, estado, ubi, marca, modelo, destino,tipo,propietario, cap, copi, az, pol,
                  altMax, cantMotor, categoria):
-        super().__init__(velocidad, cantPas, autonomia, yearFab, estado, ubi, marca, modelo, destino,tipo, cap, copi, az,
+        super().__init__(velocidad, cantPas, id,autonomia, yearFab, estado, ubi, marca, modelo, destino,tipo,propietario, cap, copi, az,
                          pol)
         self.altMax = altMax
         self.cantMotor = cantMotor
@@ -45,9 +46,9 @@ class Avion(Aeronave):
 
 
 class Helicoptero(Aeronave):
-    def __init__(self, velocidad, cantPas, autonomia, yearFab, estado, ubi, marca, modelo,destino,tipo, cap, copi, az, pol, rotores,
+    def __init__(self, velocidad, cantPas, autonomia, yearFab, estado, ubi, marca, modelo,destino,tipo,propietario, cap, copi, az, id,pol, rotores,
                  capEle, categoria):
-        super().__init__(velocidad, cantPas, autonomia, yearFab, estado, ubi, marca, modelo,destino,tipo, cap, copi, az, pol)
+        super().__init__(velocidad, cantPas, autonomia, yearFab, estado, ubi, marca, modelo,destino,tipo,propietario,cap, copi, az, pol,id)
         self.rotores = rotores
         self.capEle = capEle
         self.categoria = categoria
@@ -64,9 +65,9 @@ class Helicoptero(Aeronave):
 
 
 class Jet(Aeronave):
-    def __init__(self, velocidad, cantPas, autonomia, yearFab, estado, ubi, marca, modelo,destino,tipo, cap, copi, az, pol,
+    def __init__(self, velocidad, cantPas, autonomia, yearFab, estado, ubi, marca, modelo,destino,tipo,cap, copi, az, pol,id,
                  propietario, servicios, desFre):  # desFre == destinos frecuentes
-        super().__init__(velocidad, cantPas, autonomia, yearFab, estado, ubi, marca, modelo,destino,tipo, cap, copi, az, pol)
+        super().__init__(velocidad, cantPas, autonomia, yearFab, estado, ubi, marca, modelo,destino,tipo,propietario,cap, copi, az, pol,id)
         self.propietario = propietario
         self.servicios = "\n1.Mayordomo a bordo\n2.Spa a bordo\n3.Tripulación profesional\n4.Wi-Fi a bordo\n5.Entretenimiento a bordo\n6.Bar y bebidas\n7.Catering gourmet\n8.Conserje personal\n\n"
         self.desFre = desFre

9fafa1c4861865ff10195c91e50264c824a2fc25
aarguellocedeno <124204583+aarguellocedeno@users.noreply.github.com>
2023-11-14 08:55:58 -0500

    Update torre.py

--- avances/torre.py
+++ avances/torre.py
@@ -1,43 +1,44 @@
 import puerta
+import model
 
 
 class Torre:
     def __init__(self, name, loc):
         self.nombre = name
         self.localizacion = loc
-        self.aeronaves = []
+        self.aeronaves = model.modelo().naves
         self.embarques = []
         self.hist = []
 
     def generarPuertas(self):
         for i in range(11):
-            p = puerta()
+            p = puerta.Puerta()
             self.embarques.append(p)
+
 
     def agregarHist(self, p):
         self.hist.append(p)
 
 
     def ubicarPuerta(self, aero):
-        dispo = puerta()
+        dispo = puerta.Puerta()
         nave = aero
         flag = 0
         i = 0
-
         while((i < len(self.embarques)) and (flag == 0)):
             if(self.embarques[i].verEstado() == True):
-                embarques[i].cambiarEstado(False)
-                dispo = embarques[i]
+                self.embarques[i].cambiarEstado(False)
+                dispo = self.embarques[i]
                 flag = 1
                 aero.ubi= dispo
             else:
                 i+= 1
-        print("la nave" aero "ha sido a signada a la puerta" dispo)
+        #print("la nave" aero "ha sido a signada a la puerta" dispo)
         return(aero,dispo)
 
     def despegarAeronave(self, aeronave):
         flag,i=0,0
-        while((i<(len(self.aeronaves)))and flag = 0):
+        while((i<(len(self.aeronaves)))and flag == 0):
             if(self.aeronaves[i].verEstado()== 2):#verifica que no hayan naves en la pista
                 flag = 1
         if (flag==0):
